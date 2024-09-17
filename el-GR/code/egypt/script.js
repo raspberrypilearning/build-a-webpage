db1164f3982d39125a34e87aed786c6c9030dbc8
Sasha Mishcheriakova <135987917+sashamishcheriakova@users.noreply.github.com>
2024-09-17 17:27:48 +0100

    New translations script.js (Greek)

---
+++ el-GR/code/egypt/script.js
@@ -0,0 +1,23 @@
+//Nav li
+var button = document.getElementById('li1');
+button.addEventListener('click', function () {
+    document.location.href = 'index.html';
+});
+var button = document.getElementById('li2');
+button.addEventListener('click', function () {
+    document.location.href = 'History.html';
+});
+var button = document.getElementById('li3');
+button.addEventListener('click', function () {
+    document.location.href = 'Explore.html';
+});
+
+//btns
+var button = document.getElementById('btn1');
+button.addEventListener('click', function () {
+    document.location.href = '/Explore.html';
+});
+var button = document.getElementById('btn2');
+button.addEventListener('click', function () {
+    document.location.href = 'www.google.com/maps/place/Egypt';
+});
